32b4bd56b424b9e12f6d890163c3048bb9f0885c
congxue.zhang <congxue.zhang@qunar.com>
2018-01-22 12:02:04 +0800

    exec

--- guahao/index.js
+++ guahao/index.js
@@ -1,6 +1,6 @@
+getGHScript();
 function getGHScript() {
 	var script = document.createElement('script');
 	script.src = 'http://zcxcn.github.io/guahao/main.js';
 	document.head.appendChild(script);
 }
-getGHScript();
