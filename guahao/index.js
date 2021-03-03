f599e18e8f08a3ab23c4fa424f667eeb7aef71cb
zhangcongxue <zhangcongxue@meituan.com>
2021-03-03 15:17:14 +0800

    1

--- guahao/index.js
+++ guahao/index.js
@@ -1,6 +1,6 @@
 getGHScript();
 function getGHScript() {
 	var script = document.createElement('script');
-	script.src = 'http://zcxcn.github.io/guahao/mainV2.js?'+Date.now();
+	script.src = 'http://zcxcn.github.io/guahao/mainv2.js?'+Date.now();
 	document.head.appendChild(script);
 }
