96817ff73389e9021e55c2cb48f9241c4eeb7d0a
Iron Embers Build <build@iron-embers.invalid>
2026-09-07 18:24:21 +0800

    Deploy stronger tank damage effects

---
+++ tankbattle/assets/depth.vertex-C35uvaGk.js
@@ -0,0 +1,57 @@
+import{X as e}from"./tank-model-7GsQQrUA.js";import{a as t,i as n,n as r,o as i,r as a,s as o,t as s}from"./clipPlaneVertex-DDy_pq5z.js";import{t as c}from"./instancesDeclaration-C5nzWrtT.js";import{i as l,n as u,r as d,t as f}from"./morphTargetsVertex-B083oUSM.js";var p=`depthVertexShader`,m=`attribute position: vec3f;
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<morphTargetsVertexGlobalDeclaration>
+#include<morphTargetsVertexDeclaration>[0..maxSimultaneousMorphTargets]
+#include<clipPlaneVertexDeclaration>
+#include<instancesDeclaration>
+uniform viewProjection: mat4x4f;uniform depthValues: vec2f;
+#if defined(ALPHATEST) || defined(NEED_UV)
+varying vUV: vec2f;uniform diffuseMatrix: mat4x4f;
+#ifdef UV1
+attribute uv: vec2f;
+#endif
+#ifdef UV2
+attribute uv2: vec2f;
+#endif
+#endif
+#ifdef STORE_CAMERASPACE_Z
+uniform view: mat4x4f;varying vViewPos: vec4f;
+#endif
+varying vDepthMetric: f32;
+#define CUSTOM_VERTEX_DEFINITIONS
+@vertex
+fn main(input : VertexInputs)->FragmentInputs {var positionUpdated: vec3f=vertexInputs.position;
+#ifdef UV1
+var uvUpdated: vec2f=vertexInputs.uv;
+#endif
+#ifdef UV2
+var uv2Updated: vec2f=vertexInputs.uv2;
+#endif
+#include<morphTargetsVertexGlobal>
+#include<morphTargetsVertex>[0..maxSimultaneousMorphTargets]
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+var worldPos: vec4f=finalWorld* vec4f(positionUpdated,1.0);
+#include<clipPlaneVertex>
+vertexOutputs.position=uniforms.viewProjection*worldPos;
+#ifdef STORE_CAMERASPACE_Z
+vertexOutputs.vViewPos=uniforms.view*worldPos;
+#else
+#ifdef USE_REVERSE_DEPTHBUFFER
+vertexOutputs.vDepthMetric=((-vertexOutputs.position.z+uniforms.depthValues.x)/(uniforms.depthValues.y));
+#else
+vertexOutputs.vDepthMetric=((vertexOutputs.position.z+uniforms.depthValues.x)/(uniforms.depthValues.y));
+#endif
+#endif
+#if defined(ALPHATEST) || defined(BASIC_RENDER)
+#ifdef UV1
+vertexOutputs.vUV= (uniforms.diffuseMatrix* vec4f(uvUpdated,1.0,0.0)).xy;
+#endif
+#ifdef UV2
+vertexOutputs.vUV= (uniforms.diffuseMatrix* vec4f(uv2Updated,1.0,0.0)).xy;
+#endif
+#endif
+}
+`;e.ShadersStoreWGSL[p]||(e.ShadersStoreWGSL[p]=m);var h=[o,i,l,d,t,c,u,f,n,a,r,s];for(let t of h)e.IncludesShadersStoreWGSL[t.name]||(e.IncludesShadersStoreWGSL[t.name]=t.shader);var g={name:p,shader:m};export{g as depthVertexShaderWGSL};
--- tankbattle/assets/logDepthDeclaration-D3j9iYF3.js
+++
@@ -1,7 +0,0 @@
-import{X as e}from"./tank-model-DSiZsDh6.js";var t=`mainUVVaryingDeclaration`,n=`#ifdef MAINUV{X}
-varying vMainUV{X}: vec2f;
-#endif
-`;e.IncludesShadersStoreWGSL[t]||(e.IncludesShadersStoreWGSL[t]=n);var r={name:t,shader:n},i=`logDepthDeclaration`,a=`#ifdef LOGARITHMICDEPTH
-uniform logarithmicDepthConstant: f32;varying vFragmentDepth: f32;
-#endif
-`;e.IncludesShadersStoreWGSL[i]||(e.IncludesShadersStoreWGSL[i]=a);var o={name:i,shader:a};export{r as n,o as t};
