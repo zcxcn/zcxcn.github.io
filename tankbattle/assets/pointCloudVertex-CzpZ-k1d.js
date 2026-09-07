c3821ac6302fa3c751ecdcdda3faff408af55b36
Iron Embers Build <build@iron-embers.invalid>
2026-09-07 19:33:10 +0800

    Deploy realistic cover, tank models and battlefield materials

---
+++ tankbattle/assets/pointCloudVertex-CzpZ-k1d.js
@@ -0,0 +1,4 @@
+import{Q as e}from"./tank-model-BJhWidxI.js";var t=`pointCloudVertex`,n=`#if defined(POINTSIZE) && !defined(WEBGPU)
+gl_PointSize=pointSize;
+#endif
+`;e.IncludesShadersStore[t]||(e.IncludesShadersStore[t]=n);var r={name:t,shader:n};export{r as t};
