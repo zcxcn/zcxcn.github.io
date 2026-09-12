b4a5c61252c604d05db6aa886f154f15f3505078
Iron Embers Build <build@iron-embers.invalid>
2026-09-12 22:08:19 +0800

    Deploy game menus, six maps and eight operations

--- tankbattle/assets/rgbdDecode.fragment-B1SDqaZu.js
+++
@@ -1,5 +0,0 @@
-import{rt as e}from"./tank-model-BQuKZfCq.js";import{t}from"./helperFunctions-C42Iyd85.js";var n=`rgbdDecodePixelShader`,r=`varying vec2 vUV;uniform sampler2D textureSampler;
-#include<helperFunctions>
-#define CUSTOM_FRAGMENT_DEFINITIONS
-void main(void) 
-{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[n]||(e.ShadersStore[n]=r);var i=[t];for(let t of i)e.IncludesShadersStore[t.name]||(e.IncludesShadersStore[t.name]=t.shader);var a={name:n,shader:r};export{a as rgbdDecodePixelShader};
